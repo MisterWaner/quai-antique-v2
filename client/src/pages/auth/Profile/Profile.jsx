dc39bb6e5c9b0f793f38f0c5e9ce5218ad76f66f
Erwan Soulas <erwan.soulas@gmail.com>
2023-06-16 13:04:47 +0200

    goes on with profile UI

--- client/src/pages/auth/Profile/Profile.jsx
+++ client/src/pages/auth/Profile/Profile.jsx
@@ -43,15 +43,15 @@ export default function Profile() {
 
         await updateUser(data);
         reset();
-    }
+    };
 
     const submitPrefs = async (data, event) => {
         event.preventDefault();
         console.log(data);
 
         await updateUser(data);
-        resetPrefs()
-    }
+        resetPrefs();
+    };
 
     const submitInfoConnexion = async (data, event) => {
         event.preventDefault();
@@ -59,7 +59,7 @@ export default function Profile() {
 
         await updateUser(data);
         resetInfoConnexion();
-    }
+    };
 
     return (
         <main className="w-full flex-1 h-full md:mt-[98px] lg:container lg:mx-auto">
@@ -67,35 +67,56 @@ export default function Profile() {
                 Mon Compte
             </h1>
             <section className="my-6 px-6">
-                <h2 className="underline underline-offset-2 text-xl font-bold">Mes informations personnelles</h2>
-                <div>
-                    <form key={1} onSubmit={handleSubmit(onSubmit)}>
-                        <div>
-                            <label htmlFor="firstname">Prénom: </label>
-                            <input type="text" id="firstname" placeholder="John" />
+                <h2 className="underline underline-offset-2 text-xl font-bold">
+                    Mes informations personnelles
+                </h2>
+                <div className="w-3/4 mt-6 mx-auto flex flex-col md:justify-around lg:w-4/5 lg:flex-row">
+                    <form
+                        key={1}
+                        onSubmit={handleSubmit(onSubmit)}
+                        className="w-full md:w-[35vw] lg:w-[25vw] mb-6"
+                    >
+                        <div className="flex flex-col mb-4 w-full">
+                            <label className="text-lg pb-1" htmlFor="firstname">Prénom: </label>
+                            <input
+                                className="h-8 outline-none rounded-t-md text-base border border-b-black p-3 bg-ocre placeholder-gray-500"
+                                type="text"
+                                id="firstname"
+                                placeholder="John"
+                            />
                         </div>
-                        <div>
-                            <label htmlFor="lastname">Nom: </label>
-                            <input type="text" id="lastname" placeholder="Doe" />
+                        <div className="flex flex-col mb-4 w-full">
+                            <label className="text-lg pb-1" htmlFor="lastname">Nom: </label>
+                            <input
+                                className="h-8 outline-none rounded-t-md text-base border border-b-black p-3 bg-ocre placeholder-gray-500"
+                                type="text"
+                                id="lastname"
+                                placeholder="Doe"
+                            />
                         </div>
-                        <div>
-                            <label htmlFor="phone">Numéro de téléphone: </label>
-                            <input type="text" id="phone" placeholder="06-XX-XX-XX-XX" />
+                        <div className="flex flex-col mb-4 w-full">
+                            <label className="text-lg pb-1" htmlFor="phone">Numéro de téléphone: </label>
+                            <input
+                                className="h-8 outline-none rounded-t-md text-base border border-b-black p-3 bg-ocre placeholder-gray-500"
+                                type="text"
+                                id="phone"
+                                placeholder="06-XX-XX-XX-XX"
+                            />
                         </div>
-                        <button type="submit">Valider</button>
+                        <button className="p-2 text-lg rounded-md border border-black w-full bg-transparent font-bold hover:bg-bwn-color hover:text-white hover:text-shadow active:scale-[0.98]" type="submit">Valider</button>
                     </form>
-                    <table>
+                    <table className="w-full md:w-[35vw] lg:w-[25vw] border-2 border-black rounded-lg table-auto">
                         <tbody>
                             <tr>
-                                <td className="label">Prénom:</td>
+                                <td className="font-bold px-3">Prénom:</td>
                                 <td className="value">John</td>
                             </tr>
                             <tr>
-                                <td className="label">Nom:</td>
+                                <td className="font-bold px-3">Nom:</td>
                                 <td className="value">Doe</td>
                             </tr>
                             <tr>
-                                <td className="label">Téléphone:</td>
+                                <td className="font-bold px-3">Téléphone:</td>
                                 <td className="value">06-XX-XX-XX-XX</td>
                             </tr>
                         </tbody>
@@ -103,35 +124,45 @@ export default function Profile() {
                 </div>
             </section>
             <section className="my-6 px-6">
-                <h2 className="underline underline-offset-2 text-xl font-bold">Mes préférences: </h2>
-                <div>
-                    <form key={2}>
-                        <div>
-                            <label htmlFor="allergies">Allergies:</label>
-                            <input type="text" id="allergies" />
+                <h2 className="underline underline-offset-2 text-xl font-bold">
+                    Mes préférences:{" "}
+                </h2>
+                <div className="w-3/4 mt-6 mx-auto flex flex-col md:justify-around  lg:w-4/5 lg:flex-row">
+                    <form
+                        key={2}
+                        onSubmit={handleSubmitPrefs(submitPrefs)}
+                        className="w-full md:w-[35vw] lg:w-[25vw] mb-6"
+                    >
+                        <div className="flex flex-col mb-4 w-full">
+                            <label className="text-lg pb-1" htmlFor="allergies">Allergies:</label>
+                            <input className="h-8 outline-none rounded-t-md text-base border border-b-black p-3 bg-ocre placeholder-gray-500" type="text" id="allergies" />
                         </div>
-                        <div>
-                            <label htmlFor="quantity">Nombre de mangeurs:</label>
-                            <input type="number" id="quantity" />
+                        <div className="flex flex-col mb-4 w-full">
+                            <label className="text-lg pb-1" htmlFor="quantity">
+                                Nombre de mangeurs:
+                            </label>
+                            <input className="h-8 outline-none rounded-t-md text-base border border-b-black p-3 bg-ocre placeholder-gray-500" type="number" id="quantity" />
                         </div>
-                        <div>
-                            <label htmlFor="children">Nombre de mini-mangeurs:</label>
-                            <input type="number" id="children" />
+                        <div className="flex flex-col mb-4 w-full">
+                            <label className="text-lg pb-1" htmlFor="children">
+                                Nombre de mini-mangeurs:
+                            </label>
+                            <input className="h-8 outline-none rounded-t-md text-base border border-b-black p-3 bg-ocre placeholder-gray-500" type="number" id="children" />
                         </div>
-                        <button type="submit">Valider</button>
+                        <button className="p-2 text-lg rounded-md border border-black w-full bg-transparent font-bold hover:bg-bwn-color hover:text-white hover:text-shadow active:scale-[0.98]" type="submit">Valider</button>
                     </form>
-                    <table>
+                    <table className="w-full md:w-[35vw] lg:w-[25vw] border-2 border-black rounded-lg table-auto">
                         <tbody>
                             <tr>
-                                <td className="label">Allergies:</td>
+                                <td className="font-bold px-3">Allergies:</td>
                                 <td className="value">Noix</td>
                             </tr>
                             <tr>
-                                <td className="label">Nombre de couverts:</td>
+                                <td className="font-bold px-3">Nombre de couverts:</td>
                                 <td className="value">1</td>
                             </tr>
                             <tr>
-                                <td className="label">
+                                <td className="font-bold px-3">
                                     Nombre d&apos;enfants:
                                 </td>
                                 <td className="value">0</td>
@@ -141,22 +172,32 @@ export default function Profile() {
                 </div>
             </section>
             <section className="my-6 px-6">
-                <h2 className="underline underline-offset-2 text-xl font-bold">Mes informations de connexion: </h2>
-                <div>
-                    <form key={3}>
-                        <div>
-                            <label htmlFor="email">Email:</label>
-                            <input type="email" id="email" />
+                <h2 className="underline underline-offset-2 text-xl font-bold">
+                    Mes informations de connexion:{" "}
+                </h2>
+                <div className="w-3/4 mt-6 mx-auto flex flex-col lg:w-4/5 lg:flex-row lg:justify-start">
+                    <form
+                        key={3}
+                        onSubmit={handleSubmitInfoConnexion(
+                            submitInfoConnexion
+                        )}
+                        className="w-full md:w-[35vw] lg:w-[25vw] mb-6"
+                    >
+                        <div className="flex flex-col mb-4 w-full">
+                            <label className="text-lg pb-1" htmlFor="email">Email:</label>
+                            <input className="h-8 outline-none rounded-t-md text-base border border-b-black p-3 bg-ocre placeholder-gray-500" type="email" id="email" />
                         </div>
-                        <div>
-                            <label htmlFor="password">Mot de passe:</label>
-                            <input type="password" id="password" />
+                        <div className="flex flex-col mb-4 w-full">
+                            <label className="text-lg pb-1" htmlFor="password">Mot de passe:</label>
+                            <input className="h-8 outline-none rounded-t-md text-base border border-b-black p-3 bg-ocre placeholder-gray-500" type="password" id="password" />
                         </div>
-                        <div>
-                            <label htmlFor="confirmation">Confirmation du mot de passe:</label>
-                            <input type="password" id="confirmation" />
+                        <div className="flex flex-col mb-4 w-full">
+                            <label className="text-lg pb-1" htmlFor="confirmation">
+                                Confirmation du mot de passe:
+                            </label>
+                            <input className="h-8 outline-none rounded-t-md text-base border border-b-black p-3 bg-ocre placeholder-gray-500" type="password" id="confirmation" />
                         </div>
-                        <button type="submit">Modifier</button>
+                        <button className="p-2 text-lg rounded-md border border-black w-full bg-transparent font-bold hover:bg-bwn-color hover:text-white hover:text-shadow active:scale-[0.98]" type="submit">Modifier</button>
                     </form>
                 </div>
             </section>
